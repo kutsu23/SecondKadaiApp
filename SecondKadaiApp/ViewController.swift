08ca88e5aa653a97cf0159d4a72137bd2a7e5fc5
kutsu23 <kutsu23@gmail.com>
2019-07-26 02:16:09 +0900

    Initial Commit

---
+++ SecondKadaiApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SecondKadaiApp
+//
+//  Created by 沓掛史典 on 2019/07/26.
+//  Copyright © 2019 fuminori.kutsukake. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
